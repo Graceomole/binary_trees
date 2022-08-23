3439cc157862015a06bafee3f2e1901dff4cf994
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-08-23 22:58:35 +0100

    Create 3-binary_tree_delete.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: a pointer to the parent node of the node to create
+ * @value: the value to put in the new node
+ * Return: Always new_tree to left node
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t *nodeleft = tree->left;
+	binary_tree_t *noderighrt = tree->right;
+	if (tree == NULL)
+	{
+		;
+	}
+
+	binary_tree_delete(nodeleft);
+	binary_tree_delete(noderighrt);
+}
