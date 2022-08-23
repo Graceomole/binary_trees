aa9e611b273d4f97082d3b110377d8950d755d48
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-08-23 22:33:11 +0100

    Update 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,7 +2,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - inserts a node as the left-child of another node
+ * binary_tree_insert_right - inserts a node as the right-child of another node
  * @parent: a pointer to the parent node of the node to create
  * @value: the value to put in the new node
  * Return: Always new_tree to left node
