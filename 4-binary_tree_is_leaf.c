ebb66e8fcd3501884f720d937823ca20616dd23e
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-08-24 00:11:51 +0100

    Update 4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,10 +2,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - inserts a node as the right-child of another node
- * @parent: a pointer to the parent node of the node to create
- * @value: the value to put in the new node
- * Return: Always new_tree to left node
+ * binary_tree_is_leaf - a function that checks if a node is a leaf
+ * @node: a pointer to the node to check
+ * Return: 0 and 1 if node is a leaf
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
