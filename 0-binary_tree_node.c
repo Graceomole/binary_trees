508387bd5335e96d56ddf0d03ebb018dfc71d0b7
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-08-23 21:26:55 +0100

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,6 +11,11 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_tree = malloc(sizeof(binary_tree_t));
+
+	if (new_tree == NULL)
+	{
+		return NULL;
+	}
 	new_tree->n = value;
 	new_tree->left = NULL;
 	new_tree->right = NULL;
