e72012a7be6008ce16081f1aa48e3819db3dfee5
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-08-25 11:59:12 +0100

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,8 +17,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	}
 	else
 	{
-		leftheight = binary_tree_height(tree->left);
-		rightheight = binary_tree_height(tree->right);
+		if (tree->left)
+		{
+			leftheight += 1;
+		}
+		else if (tree->right)
+		{
+			rightheight += 1;
+		}
+		leftheight += binary_tree_height(tree->left);
+		rightheight += binary_tree_height(tree->right);
 	}
 	if (leftheight > rightheight)
 	{
