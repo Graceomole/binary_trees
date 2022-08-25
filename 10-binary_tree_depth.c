c06a4d570484ef1fe60379ef56540aec9c69b20e
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-08-25 12:02:54 +0100

    Create 10-binary_tree_depth.c

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height -  measures the height of a binary tree
+ * @tree: a pointer to the root node of the tree to traverse
+ * Return: size_t
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	binary_tree_t *current = tree->parent;
+	size_t height = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		while (current != NULL)
+		{
+			height += 1;
+			current = current->parent;
+		}
+	}
+	return (height);
+}
