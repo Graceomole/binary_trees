4c6b508653699bb88234a2084d75854e656f5890
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-08-25 18:55:24 +0100

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,7 +2,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height -  measures the height of a binary tree
+ * binary_tree_heightl -  measures the height of a binary tree
  * @tree: a pointer to the root node of the tree to traverse
  * Return: size_t
  */
@@ -16,8 +16,8 @@ int binary_tree_height(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
-	left = binary_tree_height(tree->left) + 1;
-	right = binary_tree_height(tree->right) + 1;
+	left = binary_tree_heightl(tree->left) + 1;
+	right = binary_tree_heightl(tree->right) + 1;
 
 	if (left > right)
 		return (left);
@@ -37,6 +37,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 	}
 	else
 	{
-		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+		return (binary_tree_heightl(tree->left) - binary_tree_heightl(tree->right));
 	}
 }
