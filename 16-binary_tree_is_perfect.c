b30d348f37eebdf1b7cb5a7aba49340b4e004355
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-08-25 19:28:16 +0100

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,35 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_perfect - check if a binary tree is perfect
- * @tree: pointer to root node of tree to check
- * Return: 1 if binary tree is perfect, else return 0
- */
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-	int left, right, size, pow;
-
-	if (tree == NULL)
-		return (0);
-
-	left = custom_binary_tree_height(tree->left);
-	right = custom_binary_tree_height(tree->right);
-	size = binary_tree_size(tree);
-
-	if (size == 1)
-		return (1);
-	if (left >= 0 && right >= 0 && left == right)
-	{
-		pow = get_power(left + 1);
-
-		if (pow - 1 == size)
-			return (1);
-	}
-
-	return (0);
-}
-
-/**
  * custom_binary_tree_height - Measure height of a binary tree from given node
  * @tree: pointer to node of tree to measure
  * Return: height of tree or -1 if NULL
@@ -84,4 +55,33 @@ int get_power(int exp)
 		pow *= 2;
 
 	return (pow);
+}
+
+/**
+ * binary_tree_is_perfect - check if a binary tree is perfect
+ * @tree: pointer to root node of tree to check
+ * Return: 1 if binary tree is perfect, else return 0
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int left, right, size, pow;
+
+	if (tree == NULL)
+		return (0);
+
+	left = custom_binary_tree_height(tree->left);
+	right = custom_binary_tree_height(tree->right);
+	size = binary_tree_size(tree);
+
+	if (size == 1)
+		return (1);
+	if (left >= 0 && right >= 0 && left == right)
+	{
+		pow = get_power(left + 1);
+
+		if (pow - 1 == size)
+			return (1);
+	}
+
+	return (0);
 }
